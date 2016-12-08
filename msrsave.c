80095feaca99b126454da2a5c6cef3fbd22f4073
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2016-12-08 10:52:40 -0600

    More work on msr_save().
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- msrsave.c
+++ msrsave.c
@@ -33,37 +33,43 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 #include <limits.h>
 #include <errno.h>
 #include <string.h>
 #include "msrsave.h"
 
-int msr_save(const char *out_path, const char *whitelist_path, const char *msr_path, int num_cpu)
+int msr_save(const char *out_path, const char *whitelist_path, const char *msr_path_format, int num_cpu)
 {
-    int err;
-    int i;
+    int err = 0;
+    int i, j;
+    int msr_fd;
     char err_msg[NAME_MAX];
-    size_t size;
-    size_t num_scan;
+    size_t size = 0;
+    size_t num_scan = 0;
+    size_t num_msr = 0;
     struct stat whitelist_stat;
     char *whitelist_buffer = NULL;
+    char *whitelist_ptr = NULL;
     uint64_t *msr_offset = NULL;
     uint64_t *msr_mask = NULL;
+    uint64_t *msr_save = NULL;
     FILE *whitelist_fid = NULL;
-    FILE **msr_fids = NULL;
+    FILE *save_fid = NULL;
 
     /* Figure out how big the whitelist file is */
     err = stat(whitelist_path, &whitelist_stat);
     if (err != 0) {
-        err = errno ? errno : 1;
+        err = errno ? errno : -1;
         snprintf(err_msg, NAME_MAX, "stat() of %s failed! ", whitelist_path);
         perror(err_msg);
         goto exit;
     }
 
     if (whitelist_stat.st_size == 0) {
-        err = errno ? errno : 1;
+        err = errno ? errno : -1;
         snprintf(err_msg, NAME_MAX, "Whitelist file (%s) size is zero!", whitelist_path);
         perror(err_msg);
         goto exit;
@@ -72,7 +78,7 @@ int msr_save(const char *out_path, const char *whitelist_path, const char *msr_p
     /* Allocate buffer for file contents */
     whitelist_buffer = (char*)malloc(whitelist_stat.st_size);
     if (!whitelist_buffer) {
-        err = errno ? errno : 1;
+        err = errno ? errno : -1;
         snprintf(err_msg, NAME_MAX, "Could not allocate array of size %zu!", whitelist_stat.st_size);
         perror(err_msg);
         goto exit;
@@ -90,34 +96,33 @@ int msr_save(const char *out_path, const char *whitelist_path, const char *msr_p
     /* Read contents */
     size_t num_read = fread(whitelist_buffer, 1, whitelist_stat.st_size, whitelist_fid);
     if (num_read != whitelist_stat.st_size) {
-        err = errno ? errno : 1;
+        err = errno ? errno : -1;
         snprintf(err_msg, NAME_MAX, "Contents read from whitelist file is too small: %zu < %zu!", num_read, whitelist_stat.st_size);
         perror(err_msg);
         goto exit;
     }
 
     /* Count the number of new lines in the file */
-    size_t num_whitelist_entry;
-    char *whitelist_ptr = whitelist_buffer;
-    for (num_whitelist_entry = 0;
+    whitelist_ptr = whitelist_buffer;
+    for (num_msr = 0;
          whitelist_ptr = strchr(whitelist_ptr, '\n');
-         ++num_whitelist_entry) {
-        whitelist_ptr++;
+         ++num_msr) {
+        ++whitelist_ptr;
     }
 
     /* Allocate buffers for parsed whitelist */
-    msr_offset = (uint64_t *)malloc(sizeof(uint64_t) * num_whitelist_entry);
+    msr_offset = (uint64_t *)malloc(sizeof(uint64_t) * num_msr);
     if (!msr_offset) {
-        err = errno ? errno : 1;
-        snprintf(err_msg, NAME_MAX, "Unable to allocate msr mask data of size: %zu!", sizeof(uint64_t) * num_whitelist_entry);
+        err = errno ? errno : -1;
+        snprintf(err_msg, NAME_MAX, "Unable to allocate msr offset data of size: %zu!", sizeof(uint64_t) * num_msr);
         perror(err_msg);
         goto exit;
     }
 
-    msr_mask = (uint64_t *)malloc(sizeof(uint64_t) * num_whitelist_entry);
+    msr_mask = (uint64_t *)malloc(sizeof(uint64_t) * num_msr);
     if (!msr_mask) {
-        err = errno ? errno : 1;
-        snprintf(err_msg, NAME_MAX, "Unable to allocate msr mask data of size: %zu!", sizeof(uint64_t) * num_whitelist_entry);
+        err = errno ? errno : -1;
+        snprintf(err_msg, NAME_MAX, "Unable to allocate msr mask data of size: %zu!", sizeof(uint64_t) * num_msr);
         perror(err_msg);
         goto exit;
     }
@@ -125,7 +130,7 @@ int msr_save(const char *out_path, const char *whitelist_path, const char *msr_p
     /* Parse the whitelist */
     const char *whitelist_format = "MSR: %llx Write Mask: %llx\n";
     whitelist_ptr = whitelist_buffer;
-    for (i = 0; i < num_whitelist_entry; ++i) {
+    for (i = 0; i < num_msr; ++i) {
         num_scan = sscanf(whitelist_ptr, whitelist_format, msr_offset + i, msr_mask + i);
         if (num_scan != 2) {
             err = -1;
@@ -141,19 +146,52 @@ int msr_save(const char *out_path, const char *whitelist_path, const char *msr_p
         }
     }
 
+    /* Allocate save buffer, a 2-D array over msr offset and then CPU
+       (offset major ordering) */
+    msr_save = (uint64_t *)malloc(num_msr * num_cpu * sizeof(uint64_t));
+    if (!msr_save) {
+        err = errno ? errno : -1;
+        snprintf(err_msg, NAME_MAX, "Unable to allocate msr save state buffer of size: %zu!", num_msr * num_cpu * sizeof(uint64_t));
+        perror(err_msg);
+        goto exit;
+    }
+
     /* Open all MSR files.
      * Read ALL existing data
      * Pass through the whitelist mask. */
-    msr_fids = (FILE**)malloc(sizeof(FILE*) * num_cpu);
     for (i = 0; i < num_cpu; ++i) {
         char msr_file_name[NAME_MAX];
-        snprintf(msr_file_name, NAME_MAX, msr_path, i);
-        msr_fids[i] = fopen(msr_file_name, "r");
-        if (!msr_fids[i]) {
+        snprintf(msr_file_name, NAME_MAX, msr_path_format, i);
+        msr_fd = open(msr_file_name, O_RDWR);
+        if (msr_fd == -1) {
+            err = errno ? errno : -1;
             snprintf(err_msg, NAME_MAX, "Could not open MSR file \"%s\"!", msr_file_name);
             perror(err_msg);
-            err = errno;
             goto exit;
+        }
+        for (j = 0; j < num_msr; ++j) {
+            ssize_t read_count = pread(msr_fd, msr_save + i * num_msr + j,
+                                       sizeof(uint64_t), msr_offset[j]);
+            if (read_count != sizeof(uint64_t)) {
+                err = errno ? errno : -1;
+                snprintf(err_msg, NAME_MAX, "Failed to read msr value from MSR file \"%s\"!", msr_file_name);
+                perror(err_msg);
+                goto exit;
+            }
+            msr_save[i * num_msr + j] &= msr_mask[j];
+        }
+        int close_err = close(msr_fd);
+        msr_fd = -1;
+        if (close_err) {
+            err = errno ? errno : -1;
+            snprintf(err_msg, NAME_MAX, "Could not close MSR file \"%s\"!", msr_file_name);
+            perror(err_msg);
+            goto exit;
+        }
+
+        save_fid = fopen(out_path, "w");
+        if (!save_fid) {
+            /* FIXME */
         }
     }
 
@@ -166,6 +204,9 @@ int msr_save(const char *out_path, const char *whitelist_path, const char *msr_p
 
     /* Clean up memory and files */
 exit:
+    if (msr_save) {
+        free(msr_save);
+    }
     if (msr_offset) {
         free(msr_offset);
     }
@@ -178,18 +219,13 @@ exit:
     if (whitelist_fid) {
         fclose(whitelist_fid);
     }
-    if (msr_fids){
-        for (i = 0; i < num_cpu; ++i) {
-            if (msr_fids[i]) {
-                fclose(msr_fids[i]);
-            }
-        }
-        free(msr_fids);
+    if (msr_fd != -1) {
+        close(msr_fd);
     }
     return err;
 }
 
-int msr_restore(const char *file_name, const char *whitelist_path, const char *msr_path, int num_cpu)
+int msr_restore(const char *file_name, const char *whitelist_path, const char *msr_path_format, int num_cpu)
 {
     return 0;
 }
